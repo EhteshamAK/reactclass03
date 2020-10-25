d39b4193f5c9fb7bddba8d04d5f121912d3e3cbb
Ehtesham Ali Khan <khou90525@gmail.com>
2020-10-25 09:27:55 -0700

    added default

--- src/NumberReducer.js
+++ src/NumberReducer.js
@@ -1,11 +1,14 @@
 const numberReducer = (state, action) => {
     switch (action.type){
         case 'INCREAMENT':
-            return state+1
+            return state+ 1
         case 'DECREMENT':
             return state-1
+         default:
     }
 };
+
+export default numberReducer;
 
 
 
